9981e145cdad3c37270f5179185f71f30840e279
Yang Zhuoyu <yang.zhuoyu@ist.osaka-u.ac.jp>
2020-08-03 07:29:08 +0900

    remove nested for loops

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import argparse
 import numpy as np
 import scipy.sparse as sparse
 
-from sklearn.preprocessing import normalize
+from time import time
 from scipy.sparse.linalg import spsolve
+from sklearn.preprocessing import normalize
 
 # command line parser
 parser = argparse.ArgumentParser(description='Normal Integration by mesh deformation')
@@ -15,12 +16,17 @@ parser.add_argument('normal', help='the path of normal map')
 parser.add_argument('-d', '--depth', default=None, help='the path of depth prior')
 parser.add_argument('--d_lambda', type=int, default=100, help='how much will the depth prior influence the result')
 parser.add_argument('-o', '--output', default='output', help='name of the output object and depth map')
+parser.add_argument('--vertex_depth', dest='depth_type', action='store_const',
+                    const='vertex', default='pixel', help='output vertex depth map, by default pixel depth map')
 
-def write_depth_map(filename, depth, mask_bg):
-    from scipy.signal import convolve2d
-    mask = np.array([0.25,0.25,0.25,0.25]).reshape(2,2)
-    depth = convolve2d(depth, mask, mode="valid") # keep the resolution of depth map same with the input normal map
-    depth[mask_bg] = np.nan
+def write_depth_map(filename, depth, mask, v_mask, depth_type='pixel'):
+    if depth_type == 'pixel':
+        from scipy.signal import convolve2d
+        cov_mask = np.array([0.25,0.25,0.25,0.25]).reshape(2,2)
+        depth = convolve2d(depth, cov_mask, mode="valid") # keep the resolution of depth map same with the input normal map
+        depth[~mask] = np.nan
+    else:
+        depth[~v_mask] = np.nan
     np.save(filename, depth)
 
 def write_obj(filename, d, d_ind):
@@ -48,34 +54,90 @@ def write_obj(filename, d, d_ind):
 class Normal_Integration(object):
 
     def __init__(self, nomral_path, depth_path=None, d_lambda=100): # you can tune d_lambda to decide how much the depth prior will influence the result.
-        self.n, self.mask_bg = self.read_normal_map(nomral_path) # read normal map and background mask
+        self.n, self.mask = self.read_normal_map(nomral_path) # read normal map and background mask
         self.N = None # mean-substraction matrix N defined in equation (5), "Surface-from-Gradients: An Approach Based on Discrete Geometry Processing"
         self.d_lambda = d_lambda # lambda for depth prior
 
-        if depth_path is not None:
-            self.d = np.load(depth_path)
-        else:
-            self.d = None
+        self.ilim, self.jlim = self.mask.shape # size of normal map and mask
+        self.mesh_count = (self.mask.astype(np.int32)).sum() # how many pixels in the normal map
 
-        self.ilim, self.jlim = self.mask_bg.shape[0], self.mask_bg.shape[1] # size of normal map and mask
-        self.mesh_count = np.sum((~self.mask_bg).astype(np.int32)) # how many pixels in the normal map
+        self.lu = np.pad(self.mask, [(0, 1), (0, 1)], mode='constant') # mask for left upper vertices
+        self.ru = np.pad(self.mask, [(0, 1), (1, 0)], mode='constant') # mask for right upper vertices
+        self.ld = np.pad(self.mask, [(1, 0), (0, 1)], mode='constant') # mask for left down vertices
+        self.rd = np.pad(self.mask, [(1, 0), (1, 0)], mode='constant') # mask for right down vertices
 
-        self.vertices, self.vertices_count= self.construct_vertices() # indices for the vertices
-        self.vertices_depth = np.zeros_like(self.vertices, dtype=np.float32) # depth for the vertices
+        self.v_mask = self.lu|self.ru|self.ld|self.rd # mask for all vertices
+        self.v_count = (self.v_mask.astype(np.int32)).sum() # total number of all vertices
+
+        self.v_index = np.zeros_like(self.v_mask, dtype='uint') # indices for all vertices
+        self.v_index[self.v_mask] = np.arange(self.v_count)+1
+
+        self.v_depth = np.zeros_like(self.v_index, dtype=np.float32) # depth for the vertices
+
+        self.A, self.b = self.construct_Ax_b_without_depth_prior()
 
         # below we are going to solve NAx=Nb, let's construct them first
-        self.A = self.construct_A().tocsc()
-        self.N = self.construct_N().tocsc()
-        self.b = self.construct_b()
+        self.N = self.construct_N()
         self.NA = self.N@self.A
         self.Nb = self.N@self.b
-        
-        if self.d is not None:
-            self.add_depth_prior()
+
+        if depth_path is not None: # add depth prior
+            A, b = self.add_depth_prior(depth_path)
+            self.NA = sparse.vstack([self.NA, A])
+            self.Nb = np.vstack([self.Nb, b])
 
         # since NA is a tall matrix, we use normal equation NA.T@NA@x=NA.T@Nb to solve this linear system
         self.NATNA = self.NA.T@self.NA
         self.NATNb = self.NA.T@self.Nb
+
+    def add_depth_prior(self, depth_path):
+
+        d = np.load(depth_path) # read depth prior
+        d_mask = ~np.isnan(d) # mask for depth prior
+        d_count = (d_mask.astype(np.int32)).sum() # total number of depth prior
+
+        # scipy.sparse.coo_matrix((data, (row, col))
+        # build row for sparse coo matrix
+        row = np.arange(d_count).repeat(4)
+
+        # build col for sparse coo matrix
+        lu = np.pad(d_mask, [(0, 1), (0, 1)], mode='constant') # mask for left upper vertices
+        ru = np.pad(d_mask, [(0, 1), (1, 0)], mode='constant') # mask for right upper vertices
+        ld = np.pad(d_mask, [(1, 0), (0, 1)], mode='constant') # mask for left down vertices
+        rd = np.pad(d_mask, [(1, 0), (1, 0)], mode='constant') # mask for right down vertices
+
+        col = np.vstack((self.v_index[lu],self.v_index[ru],self.v_index[ld],self.v_index[rd])).T.ravel()
+
+        # build data for sparse coo matrix
+        data = np.ones(4*d_count)
+        data = 1/4*data*self.d_lambda
+        
+        # build A for Ax=b
+        A = sparse.coo_matrix((data, (row, col)), shape=(d_count, self.v_count)).tocsc()
+        # build b for Ax=b
+        b = d[d_mask].reshape(-1,1)*self.d_lambda
+        
+        return A, b
+
+    def construct_Ax_b_without_depth_prior(self):
+
+        # scipy.sparse.coo_matrix((data, (row, col))
+        # build row for sparse coo matrix
+        row = np.arange(4*self.mesh_count)
+        # build col for sparse coo matrix
+        col = np.vstack((self.v_index[self.lu],self.v_index[self.ru],self.v_index[self.ld],self.v_index[self.rd])).T.ravel()
+        # build data for sparse coo matrix
+        data = np.ones(4*self.mesh_count)
+
+        x = self.n[...,0][self.mask]
+        y = self.n[...,1][self.mask]
+        z = -self.n[...,2][self.mask]
+        # build A for Ax=b
+        A = sparse.coo_matrix((data, (row, col-1)), shape=(4*self.mesh_count, self.v_count)).tocsc()
+        # build b for Ax=b
+        b = np.vstack(((x/-2+y/2)/z,(x/2+y/2)/z,(x/-2+y/-2)/z,(x/2+y/-2)/z)).T.reshape(-1, 1)
+
+        return A, b
 
     def read_normal_map(self, path):
         '''
@@ -87,7 +149,7 @@ class Normal_Integration(object):
 
         output:
             n: normalized normal map
-            mask_bg: background mask
+            mask: normal map mask
         '''
 
         if ".npy" in path:
@@ -110,62 +172,7 @@ class Normal_Integration(object):
 
         # fill background with [0,0,0]
         n[mask_bg] = [0,0,0]
-        return n, mask_bg
-
-    def construct_vertices(self):
-        count = 0
-        vertices = np.zeros((self.ilim+1,self.jlim+1), dtype="uint")
-        for i in range(self.ilim):
-            for j in range(self.jlim):
-                if ~self.mask_bg[i, j]:
-                    if vertices[i, j] == 0:
-                        count += 1
-                        vertices[i, j] = count
-                    if vertices[i, j+1] == 0:
-                        count += 1
-                        vertices[i, j+1] = count
-                    if vertices[i+1, j] == 0:
-                        count += 1
-                        vertices[i+1, j] = count
-                    if vertices[i+1, j+1] == 0:
-                        count += 1
-                        vertices[i+1, j+1] = count
-        return vertices, count
-
-    def construct_A(self):
-        row = range(4*self.mesh_count)
-        col = []
-        data = [1 for i in row]
-        for i in range(self.ilim):
-            for j in range(self.jlim):
-                if ~self.mask_bg[i, j]:
-                    col.append(self.vertices[i,j]-1)
-                    col.append(self.vertices[i,j+1]-1)
-                    col.append(self.vertices[i+1,j]-1)
-                    col.append(self.vertices[i+1,j+1]-1)
-
-        A = sparse.coo_matrix((data, (row, col)), shape=(4*self.mesh_count, self.vertices_count))
-
-        return A
-
-    def construct_b(self):
-        b = []
-        for i in range(self.ilim):
-            for j in range(self.jlim):
-                if ~self.mask_bg[i, j]:
-                    if self.n[i,j,2]<=0.0871557:
-                        b.append(self.vertices_depth[i,j])
-                        b.append(self.vertices_depth[i,j+1])
-                        b.append(self.vertices_depth[i+1,j])
-                        b.append(self.vertices_depth[i+1,j+1])
-                    else:
-                        b.append(-(self.n[i,j,0]/-2+self.n[i,j,1]/2)/self.n[i,j,2])
-                        b.append(-(self.n[i,j,0]/2+self.n[i,j,1]/2)/self.n[i,j,2])
-                        b.append(-(self.n[i,j,0]/-2+self.n[i,j,1]/-2)/self.n[i,j,2])
-                        b.append(-(self.n[i,j,0]/2+self.n[i,j,1]/-2)/self.n[i,j,2])
-
-        b = np.array(b).reshape(-1, 1)
-        return b
+        return n, ~mask_bg
 
     def construct_N(self):
         N_ = np.eye(4)-0.25
@@ -173,45 +180,28 @@ class Normal_Integration(object):
         indptr = range(self.mesh_count+1)
         indices = range(self.mesh_count)
         N = sparse.bsr_matrix((data,indices,indptr))
-        return N
-
-    def add_depth_prior(self):
-        row = 0
-        col = []
-        b = []
-        for i in range(self.ilim):
-            for j in range(self.jlim):
-                if ~np.isnan(self.d[i,j]):
-                    row += 1
-                    col.append(self.vertices[i,j]-1)
-                    col.append(self.vertices[i,j+1]-1)
-                    col.append(self.vertices[i+1,j]-1)
-                    col.append(self.vertices[i+1,j+1]-1)
-                    b.append(self.d_lambda*self.d[i,j])
-        row = [i//4 for i in range(row*4)]
-        data = [self.d_lambda*1/4 for i in row]
-        A = sparse.coo_matrix((data, (row, col)), shape=(len(row)//4, self.vertices_count))
-        b = np.array(b).reshape(-1, 1)
-        self.NA = sparse.vstack([self.NA, A])
-        self.Nb = np.vstack([self.Nb, b])
+        return N.tocsc()
 
     def mesh_deformation(self):
         x = spsolve(self.NATNA, self.NATNb) # solve NATNAx = NATNb by SciPy
-        for i in range(self.ilim+1):
-            for j in range(self.jlim+1):
-                if self.vertices[i, j] != 0:
-                    self.vertices_depth[i, j] = x[self.vertices[i, j]-1] # since x is vertorized depth, we need to put it back to matrix form by using indices of vertices
+        self.v_depth[self.v_mask] = x
 
 if __name__ == '__main__':
+
     args = parser.parse_args()
+
+    start = time()
 
     print("Start reading input data...")
     task = Normal_Integration(args.normal, args.depth, args.d_lambda)
     print("Start normal integration...")
     task.mesh_deformation()
 
+    end = time()
+    print("Time elapsed: {:0.2f}".format(end - start))
+
     print("Start writing obj file...")
-    write_obj("{0}.obj".format(args.output), task.vertices_depth, task.vertices) # write obj file
+    write_obj("{0}.obj".format(args.output), task.v_depth, task.v_index) # write obj file
     print("Start writing depth map...")
-    write_depth_map("{0}_depth.npy".format(args.output), task.vertices_depth, task.mask_bg) # write depth file
+    write_depth_map("{0}_depth.npy".format(args.output), task.v_depth, task.mask, task.v_mask, depth_type=args.depth_type) # write depth file
     print("Finish!")
